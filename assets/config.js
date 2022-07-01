267e971a443e93c6d7bbeeb858d17b197418fa50
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-06-30 22:39:33 -0300

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,38 @@
+const config = {
+  USERNAME: "Zaeli#9835",
+
+  PICTURE: "assets/img/profile_picture.gif",
+  HEADER: "assets/img/header.gif",
+
+  DESCRIPTION:
+    "Hi, I'm Zaeli, a 20 years old young Brazillian starter programmer and gamer in my free time.",
+
+  BADGES: [
+    { id: "Python Starter", image: "assets/img/python.png" },
+    { id: "Dev Starter", image: "assets/img/devbadge.png" },
+    { id: "VSCode", image: "assets/img/vscode.png" },
+    { id: "Terminal", image: "assets/img/terminal.png" },
+  ],
+
+  PROJECTS: [
+    {
+      id: "project-1",
+      image: "assets/img/guessgame.png",
+      link: "https://github.com/Kurumin7-gif/juegos/",
+      name: "Guess the Number+Guess the Word",
+    },
+    {
+      id: "project-2",
+      image: "assets/img/minizeca.png",
+      link: "https://kurumin7-gif.github.io/gang-do-zeca-discord/",
+      name: "Gang do Zeca's Page",
+    },
+  ],
+
+  SOCIALS: [
+    { class_name: "fab fa-github", link: "https://github.com/Kurumin7-gif" },
+    { class_name: "fab fa-twitter", link: "https://twitter.com/zaaeeli" },
+    { class_name: "fab fa-twitch", link: "https://www.twitch.tv/zaeeli" },
+    { class_name: "fab fa-instagram", link: "https://www.instagram.com/disraelifilho/"}
+  ],
+};
